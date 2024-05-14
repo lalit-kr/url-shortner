e1420319773632bd2993e6430f1d4e38f67ac464
jyzib <jazibzaidi02@gmail.com>
2024-05-14 12:08:58 +0530

    feat: Add bcrypt for password hashing

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,16 @@
 const USER = require(__dirname + "/../models/user");
-const { setUser } = require(__dirname + "/../service/auth");
-
+const { setUser } = require("../service/auth");
+const bcrypt = require("bcrypt");
+// const { setUser } = require(__dirname + "/../service/auth");
+const saltRounds = 10;
 console.log(__dirname + "/../service/auth");
 
 async function userSignup(req, res) {
   try {
     const { name, email, password } = req.body;
-    const user = await USER.create({ name, email, password });
+    const hash_password = await bcrypt.hash(password, saltRounds);
+
+    const user = await USER.create({ name, email, password: hash_password });
 
     const token = setUser(user);
     setUser(user);
@@ -23,16 +27,21 @@ async function userSignup(req, res) {
 async function userLogin(req, res) {
   try {
     const { email, password } = req.body;
-    const user = await USER.findOne({ email, password });
-    if (!user)
-      return res.render("login", { error: "Invalid email or password!" });
+
+    const user = await USER.findOne({ email });
+
+    if (!user) return res.render("login", { error: "Invalid email " });
+
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) {
+      return res.render("login", { error: "Invalid password " });
+    }
 
     const token = setUser(user);
     setUser(user);
     res.cookie("token", token);
     return res.status(200).redirect("/");
   } catch (err) {
-    console.warn(err);
     return res
       .status(500)
       .render("login", { error: "Server Error! Please try again." });
